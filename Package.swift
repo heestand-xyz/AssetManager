6d8ecb09835fda965c463c2b4dcba2f497f9b7d1
Anton Heestand <anton.heestand@gmail.com>
2025-02-15 20:28:15 +0900

    Packages

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/heestand-xyz/MultiViews", from: "3.0.0"),
-        .package(url: "https://github.com/heestand-xyz/TextureMap", from: "2.0.0"),
+        .package(url: "https://github.com/heestand-xyz/TextureMap", from: "2.1.0"),
     ],
     targets: [
         .target(
